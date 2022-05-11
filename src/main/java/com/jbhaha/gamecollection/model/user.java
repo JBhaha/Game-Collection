a5d902e3a63ccfc14d87beab8a2d553ebe72c338
Jakub Korona <koronaj@bzz.ch>
2022-05-11 16:01:12 +0200

    Created user

---
+++ src/main/java/com/jbhaha/gamecollection/model/user.java
@@ -0,0 +1,41 @@
+package com.jbhaha.gamecollection.model;
+
+public class user {
+
+    private String userUUID;
+    private String userName;
+    private String password;
+    private String userRole;
+
+    public String getUserUUID() {
+        return userUUID;
+    }
+
+    public void setUserUUID(String userUUID) {
+        this.userUUID = userUUID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}
